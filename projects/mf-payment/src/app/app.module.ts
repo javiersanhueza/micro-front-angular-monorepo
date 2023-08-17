37139b29f80f7a6e7223e114b3be65d490f427d5
Javier Sanhueza Mejías <javier.sanhueza@nextcode.cl>
2023-08-17 13:31:46 -0400

    Cargando Micro FrontEnds en Lazy Loading

--- projects/mf-payment/src/app/app.module.ts
+++ projects/mf-payment/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, PaymentComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ]
 })
 export class AppModule { }
